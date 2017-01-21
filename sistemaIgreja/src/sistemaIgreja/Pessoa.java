460af4ea0e80a2ff222372203b40e27dd0c75f36
ArturAquino <uchihaartur@yahoo.com.br>
2017-01-21 14:12:57 -0200

    Inicio dos trabalhos

---
+++ sistemaIgreja/src/sistemaIgreja/Pessoa.java
@@ -0,0 +1,15 @@
+package sistemaIgreja;
+
+import java.util.Date;
+
+public abstract class Pessoa {
+	
+	private String nome;
+	private String nomeQuadrante;
+	private String nomeCracha;
+	private Long estadoCivil;
+	private Date dataNascimento;
+	private Date dataCasamento;
+	private String sexo;
+
+}
