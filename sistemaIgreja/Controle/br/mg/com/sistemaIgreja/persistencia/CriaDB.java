3215c93564fefd6e174508accce1e41a4828aadc
Valderi <Valderi@10.31.87.251>
2017-02-04 17:05:59 -0200

    commit

--- sistemaIgreja/Controle/br/mg/com/sistemaIgreja/persistencia/CriaDB.java
+++ sistemaIgreja/Controle/br/mg/com/sistemaIgreja/persistencia/CriaDB.java
@@ -1,71 +1,37 @@
 package br.mg.com.sistemaIgreja.persistencia;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.mg.com.classe.Cliente;
 
 public class CriaDB {
 	
-	private static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
-	private static final String JDBC_URL = "jdbc:derby:sistemaIgrejaDB;create=true";
+//	private static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+//	private static final String JDBC_URL = "jdbc:derby:sistemaIgrejaDB;create=true";
+//	
+//	private static final String SQL_STATEMENT = "SELECT * FROM TESTE";
 	
-	private static final String SQL_STATEMENT = "SELECT * FROM TESTE";
-	
-	public static void main(String[] args){
-		 
-		try {
-			
-			Class.forName(DRIVER);
-			Connection con = DriverManager.getConnection(JDBC_URL);
-			
-			/*Comando para criação de uma tabela*/
-			/*con.createStatement().execute(""
-					+ "CREATE TABLE TESTE("
-					+ "		NOME VARCHAR(255),"
-					+ "		SEXO VARCHAR(1),"
-					+ "		IDADE INTEGER)"
-					);*/
-			/*Comando para inserção de dados em uma tabela*/
-			/*con.createStatement().execute(""
-					+ "INSERT INTO TESTE(NOME, SEXO, IDADE)"
-					+ "		VALUES('ADEMIR', 'M', 29)");
-			
-			//System.out.println("Tabela criada com sucesso!!!");*/
-			
-			Statement statemant = con.createStatement();
-			
-			ResultSet res = statemant.executeQuery(SQL_STATEMENT);
-			
-			//ResultSetMetaData resMD = res.getMetaData();
-			
-			//int totalColuna = resMD.getColumnCount(); 
-			
-			for(int i = 1; i <= res.getMetaData().getColumnCount(); i++){
-				System.out.format("%20s", res.getMetaData().getColumnName(i)+" | ");
-			}
-			
-			while(res.next()){
-				
-				System.out.println();
-				
-				for(int i = 1; i <= res.getMetaData().getColumnCount(); i++){
-					System.out.format("%20s", res.getString(i)+" | ");
-				}
-			}
-			
-			if(statemant != null)
-				statemant.close();
-			
-			if(con != null)
-				con.close();
-			
-			
-		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Derby");
+		EntityManager em = emf.createEntityManager();
+
+		Cliente cliente = new Cliente();
+		cliente.setNome("Pedro Bino");
+		cliente.setIdade(35);
+		cliente.setSexo("M");
+
+		em.getTransaction().begin();
+		em.persist(cliente);
+		em.getTransaction().commit();
+
+		System.out.println("Cliente salvo com sucesso !!");
+
+		//em.close();
+
 		}
-		
-	}
+
 
 }
